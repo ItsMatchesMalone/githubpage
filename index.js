26afc4b4cc271f17c0d994f7ce94bc953f7bdf85
ItsMatchesMalone <pelafeco@gmail.com>
2024-08-06 04:16:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
 let helado ="chocolate";
 if(helado=== "chocolate"){
-    alert("Hola Luci");
+    alert("Bienvenido a PELUVERSE");
 }
 else{
     alert("mala eleccion");
@@ -10,3 +11,19 @@ function alertarpeluverse(){
     alert('SÍ, ESTÁS EN PELUVERSE');
 }
 document.querySelector('h1').onclick = alertarpeluverse;
+
+function alertarmejor(){
+    alert('la mejor entre comillas');
+}
+document.querySelector('h2').onclick = alertarmejor;
+
+let miimagen = document.querySelector('img');
+miimagen.onclick=function(){
+    let src = miimagen.getAttribute('src');
+    if(src === 'fotosweb/angelodemoni.jpg' ){
+        miimagen.setAttribute('src','fotosweb/demoniangel.jpg');
+    }
+    else{
+        miimagen.setAttribute('src','fotosweb/angelodemoni.jpg');
+    }
+}
